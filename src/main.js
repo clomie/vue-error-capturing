bc4c2f9e12512a7a4df1044b50bc4227e69b49e0
clomie <cclomie@gmail.com>
2018-09-25 16:58:57 +0900

    Update

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 Vue.config.errorHandler = (err, vm, info) => {
-  console.log(`Captured in Vue.config.errorHandler: ${info} ${err}`);
+  console.log(`Captured in Vue.config.errorHandler: ${info}`, err);
 };
 Vue.config.warnHandler = (msg, vm, trace) => {
   console.log(`Captured in Vue.config.warnHandler\n${msg}${trace}`);
 };
 window.addEventListener("error", event => {
-  console.log(`Captured in error EventListener: ${event.error}`);
+  console.log("Captured in error EventListener", event.error);
 });
 window.addEventListener("unhandledrejection", event => {
-  console.log(`Captured in unhandledrejection EventListener: ${event.reason}`);
+  console.log("Captured in unhandledrejection EventListener", event.reason);
 });
 
 new Vue({
